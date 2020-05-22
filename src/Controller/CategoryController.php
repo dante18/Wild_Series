cc511d84f325b3f0ba14657466a632030abe5d77
dante18 <lablandinerie2@gmail.com>
2020-05-22 17:29:07 +0200

    Processing the form

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,9 +21,29 @@ class CategoryController extends AbstractController
 
     /**
      * @Route("/category/add", name="category_add")
+     * @param Request $request
+     * @return Response
      */
-    public function add() :Response
+    public function add(Request $request) :Response
     {
-        return $this->render('category/add.html.twig');
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category
+        );
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $data = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($data);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('category_add');
+        }
+
+        return $this->render('category/add.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
