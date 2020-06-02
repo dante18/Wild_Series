62765331c349e56d3d8587a70d2b576a85865aa5
dante18 <lablandinerie2@gmail.com>
2020-06-02 12:01:21 +0200

    Update file ActorFixtures.php

--- src/DataFixtures/ActorFixtures.php
+++ src/DataFixtures/ActorFixtures.php
@@ -1,15 +1,16 @@
 <?php
 
-
 namespace App\DataFixtures;
 
 use App\Entity\Actor;
+use Faker;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class ActorFixtures extends Fixture implements DependentFixtureInterface
 {
+
     const ACTORS = [
         'Andrew Lincoln',
         'Norman Reedus',
@@ -17,8 +18,18 @@ class ActorFixtures extends Fixture implements DependentFixtureInterface
         'Victoria Pedretti',
     ];
 
+
+
     public function load(ObjectManager $manager)
     {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i=0; $i<10; $i++) {
+            $actor = new Actor();
+            $actor->setName($faker->name);
+            $manager->persist($actor);
+            $actor->addProgram($this->getReference('program_' . rand(0, 3)));
+        }
 
         foreach (self::ACTORS as $key => $actorName) {
             $actor = new Actor();
