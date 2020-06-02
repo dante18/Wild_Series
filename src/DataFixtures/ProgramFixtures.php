4e27b3d3857c7d537638b0a747973337d9906e39
dante18 <lablandinerie2@gmail.com>
2020-06-02 12:10:11 +0200

    Update file ProgramFixtures.php

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Program;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
