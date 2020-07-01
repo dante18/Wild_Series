a5636a3fcda777ff116a8059f105067a4db92068
dante18 <lablandinerie2@gmail.com>
2020-07-01 17:53:19 +0200

    Update of program page

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,12 @@ Encore
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
+    .copyFiles({
+        from: './assets/img',
+
+        // optional target path, relative to the output dir
+        to: 'img/[path][name].[ext]',
+    })
 
     /*
      * ENTRY CONFIG
